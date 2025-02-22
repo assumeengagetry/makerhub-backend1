8c60e0518580a022cad010f0b77f7e81c819a46e
assumeengagetry <xsk13969662263@outlook.com>
2025-02-23 07:01:31 +0800

    把一些用不到的删掉

--- scripts/maintenance/cleanup_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LOG_DIR="/var/log/society-manager"
-
-# 删除30天前的日志
-find $LOG_DIR -type f -name "*.log" -mtime +30 -delete
-find $LOG_DIR -type f -name "*.zip" -mtime +30 -delete
-
-echo "已清理30天前的日志文件"
--- tests/intergration/mongo_db.py
+++
@@ -1,3 +0,0 @@
-from mongoengine import connect
-
-connect('makerhub_db', host='mongodb://localhost:27017/makerhub_db')
